1e974f9f93ab270a412c5fc446d110d1a2a2613b
Andrew Hopkins <andhop@amazon.com>
2023-05-02 17:07:34 -0700

    Add toolchain

---
+++ util/powerpc-toolchain.cmake
@@ -0,0 +1,7 @@
+# Specify the target system
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR powerpc)
+
+# Specify the cross-compiler
+set(CMAKE_C_COMPILER powerpc-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER powerpc-linux-gnu-g++)
