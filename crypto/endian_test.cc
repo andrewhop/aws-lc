0f1b55f8493b05d3447d95876ab4e27fa8aea102
Andrew Hopkins <andhop@amazon.com>
2023-07-18 18:13:31 -0700

    Add endian test for helper functions

---
+++ crypto/endian_test.cc
@@ -0,0 +1,108 @@
+#include <gtest/gtest.h>
+#include "internal.h"
+#include "test/test_util.h"
+
+
+TEST(EndianTest, u32Operations) {
+  uint8_t buffer[4];
+  uint32_t val = 0x12345678;
+  uint8_t expected_be[4] = {0x12, 0x34, 0x56, 0x78};
+  uint8_t expected_le[4] = {0x78, 0x56, 0x34, 0x12};
+
+
+  CRYPTO_store_u32_le(buffer, val);
+  EXPECT_EQ(Bytes(expected_le), Bytes(buffer));
+  EXPECT_EQ(val, CRYPTO_load_u32_le(buffer));
+
+  CRYPTO_store_u32_be(buffer, val);
+  EXPECT_EQ(Bytes(expected_be), Bytes(buffer));
+  EXPECT_EQ(val, CRYPTO_load_u32_be(buffer));
+}
+
+TEST(EndianTest, u64Operations) {
+  uint8_t buffer[8];
+  uint64_t val = 0x123456789abcdef0;
+  uint8_t expected_be[8] = {0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0};
+  uint8_t expected_le[8] = {0xf0, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12};
+
+  CRYPTO_store_u64_le(buffer, val);
+  EXPECT_EQ(Bytes(expected_le), Bytes(buffer));
+  EXPECT_EQ(val, CRYPTO_load_u64_le(buffer));
+
+  CRYPTO_store_u64_be(buffer, val);
+  EXPECT_EQ(Bytes(expected_be), Bytes(buffer));
+  EXPECT_EQ(val, CRYPTO_load_u64_be(buffer));
+}
+
+TEST(EndianTest, wordOperations) {
+  uint8_t buffer[sizeof(size_t)];
+#if defined(OPENSSL_64_BIT)
+  size_t val = 0x123456789abcdef0;
+  uint8_t expected_le[8] = {0xf0, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12};
+#else
+size_t val = 0x12345678;
+uint8_t expected_le[4] = {0x78, 0x56, 0x34, 0x12};
+#endif
+
+  CRYPTO_store_word_le(buffer, val);
+  EXPECT_EQ(Bytes(expected_le), Bytes(buffer));
+  EXPECT_EQ(val, CRYPTO_load_word_le(buffer));
+}
+
+TEST(EndianTest, TestRotate32) {
+  uint32_t value = 0b00000010000000000000000000000;
+  // 0x12345678  = 0b10010001101000101011001111000
+  // Rotate 4    = 0b00110100010101100111100010010
+  uint32_t expected = 0b00100000000000000000000000000;
+
+  uint32_t rotl_by = 4;
+  uint32_t rotr_by = 32 - rotl_by;
+
+  uint32_t rotl_value = CRYPTO_rotl_u32(value, rotl_by);
+  uint32_t rotr_value = CRYPTO_rotr_u32(value, rotr_by);
+
+  ASSERT_EQ(rotl_value, rotr_value);
+  EXPECT_EQ(expected, rotl_value);
+  ASSERT_EQ(CRYPTO_rotr_u32(rotl_value, rotl_by), value);
+}
+
+TEST(EndianTest, TestRotate64) {
+  uint64_t value = 0b0000001000000000000000000000000000010000000000000000000000;
+  uint64_t expected = 0b0010000000000000000000000000000100000000000000000000000000;
+
+  uint64_t rotl_by = 4;
+  uint64_t rotr_by = 64 - rotl_by;
+
+  uint64_t rotl_value = CRYPTO_rotl_u64(value, rotl_by);
+  uint64_t rotr_value = CRYPTO_rotr_u64(value, rotr_by);
+
+  ASSERT_EQ(rotl_value, rotr_value);
+  EXPECT_EQ(expected, rotl_value);
+  ASSERT_EQ(CRYPTO_rotr_u64(rotl_value, rotl_by), value);
+}
+
+union test_union {
+  uint32_t big;
+  uint16_t small;
+};
+
+struct test_struct {
+  test_union union_val;
+};
+
+TEST(EndianTest, TestStructUnion) {
+  struct test_struct val = {{0}};
+  val.union_val.big = 0x12345678;
+
+  uint32_t big_val = val.union_val.big;
+  uint16_t small_val = val.union_val.small;
+
+#if defined(OPENSSL_BIG_ENDIAN)
+  ASSERT_EQ(small_val, (uint16_t)0x1234);
+
+#else
+  ASSERT_EQ(small_val, (uint16_t)0x5678);
+#endif
+  // Assert that the big value remains as expected
+  ASSERT_EQ(big_val, (uint32_t)0x12345678);
+}
