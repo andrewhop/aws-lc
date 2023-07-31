27f5f6402108535cdb358bcc970f8a74b69dc88b
Andrew Hopkins <andhop@amazon.com>
2023-07-31 14:29:30 -0700

    Always include an empty prefix

---
+++ include/openssl/boringssl_prefix_symbols.h
@@ -0,0 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR ISC
+
+// This file is intentionally empty, when building with the prefix enabled the
+// generated file will overwrite this one.
